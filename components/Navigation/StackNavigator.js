b652b34de8dca648865e17a5dc705379b280c234
feheed <fmjuwaihel@gmail.com>
2022-02-24 08:00:08 +0300

    done sign in / up

--- components/Navigation/StackNavigator.js
+++ components/Navigation/StackNavigator.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import Home from "../Home";
+import SignUp from "../Auth/SignUp";
 
 const StackNavigator = () => {
   const { Navigator, Screen } = createStackNavigator();
 
   return (
-    <Navigator initialRouteName="Home">
+    <Navigator initialRouteName="SignUp">
       <Screen name="Home" component={Home} />
+      <Screen name="SignUp" component={SignUp} />
     </Navigator>
   );
 };
--- components/auth/SignUp.js
+++ components/auth/SignUp.js
@@ -1,60 +1,204 @@
-import React from "react";
-import {
-  VStack,
-  Center,
-  Box,
-  Heading,
-  FormControl,
-  Input,
-  Button,
-  StyleSheet,
-} from "native-base";
+//Icons
+import React, { useState } from "react";
+import Icon from "react-native-vector-icons/Ionicons";
+import IconLock from "react-native-vector-icons/Feather";
 
-const Signup = () => {
+// react native import
+import {
+  StyleSheet,
+  View,
+  Image,
+  ImageBackground,
+  Text,
+  Dimensions,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
+
+import { observer } from "mobx-react-lite";
+import authStore from "../../stores/authStore";
+
+const { width: WIDTH } = Dimensions.get("window");
+const Home = () => {
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+    email: "",
+    age: null,
+    nationality: "",
+    bio: "",
+    image: "",
+  });
+  const handlesubmit = () => {
+    console.log(user);
+    authStore.signUp(user);
+    // \\call sign in functioon from auth store
+  };
   return (
-    <Center w="100%">
-      <Box safeArea p="2" w="90%" maxW="290" py="8">
-        <Heading
-          size="lg"
-          color="coolGray.800"
-          _dark={{
-            color: "warmGray.50",
+    <ImageBackground
+      source={{
+        uri: "https://media.idownloadblog.com/wp-content/uploads/2018/12/snow-winter-car-night-nature-iphone-X.jpg",
+      }}
+      style={styles.backgroundContainer}
+    >
+      <View style={styles.logoContainer}>
+        <Image
+          source={{
+            uri: "https://icon-library.com/images/google-location-icon/google-location-icon-17.jpg",
           }}
-          fontWeight="semibold"
-        >
-          Welcome
-        </Heading>
-        <Heading
-          mt="1"
-          color="coolGray.600"
-          _dark={{
-            color: "warmGray.200",
-          }}
-          fontWeight="medium"
-          size="lg"
-        >
-          Sign up to continue!
-        </Heading>
-        <VStack space={8} mt="9">
-          <FormControl>
-            <FormControl.Label>Email</FormControl.Label>
-            <Input />
-          </FormControl>
-          <FormControl>
-            <FormControl.Label>Password</FormControl.Label>
-            <Input type="password" />
-          </FormControl>
-          <FormControl>
-            <FormControl.Label>Confirm Password</FormControl.Label>
-            <Input type="password" />
-          </FormControl>
-          <Button mt="2" colorScheme="indigo">
-            Sign up
-          </Button>
-        </VStack>
-      </Box>
-    </Center>
+          style={styles.logo}
+        />
+        <Text style={styles.logoText}>Tripsy</Text>
+      </View>
+      <View style={styles.logoContainer}>
+        <Icon
+          style={styles.inputIcon}
+          name={"ios-person-outline"}
+          size={28}
+          color={"rgba(255,255,255,0.7)"}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder={"Username"}
+          onChangeText={(value) => setUser({ ...user, username: value })}
+          placeholderTextColor={"rgba(255,255,255,07)"}
+          underlineColorAndroid={"transparent"}
+        />
+      </View>
+      <View style={styles.logoContainer}>
+        <IconLock
+          style={styles.inputIcon}
+          name={"lock"}
+          size={28}
+          color={"rgba(255,255,255,0.7)"}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder={"Password"}
+          onChangeText={(value) => setUser({ ...user, password: value })}
+          secureTextEntry={true}
+          placeholderTextColor={"rgba(255,255,255,07)"}
+          underlineColorAndroid={"transparent"}
+        />
+      </View>
+      <View style={styles.logoContainer}>
+        <Icon
+          style={styles.inputIcon}
+          name={"md-person-circle-outline"}
+          size={28}
+          color={"rgba(255,255,255,0.7)"}
+        />
+
+        <TextInput
+          style={styles.input}
+          placeholder={"Profile Picture"}
+          onChangeText={(value) => setUser({ ...user, image: value })}
+          placeholderTextColor={"rgba(255,255,255,07)"}
+          underlineColorAndroid={"transparent"}
+        />
+      </View>
+      <View style={styles.logoContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder={"Example@example.com"}
+          onChangeText={(value) => setUser({ ...user, email: value })}
+          placeholderTextColor={"rgba(255,255,255,07)"}
+          underlineColorAndroid={"transparent"}
+        />
+      </View>
+      <View style={styles.logoContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder={"age 1-200"}
+          onChangeText={(value) => setUser({ ...user, age: value })}
+          placeholderTextColor={"rgba(255,255,255,07)"}
+          underlineColorAndroid={"transparent"}
+        />
+      </View>
+      <View style={styles.logoContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder={"Nationality"}
+          onChangeText={(value) => setUser({ ...user, nationality: value })}
+          placeholderTextColor={"rgba(255,255,255,07)"}
+          underlineColorAndroid={"transparent"}
+        />
+      </View>
+      <View style={styles.logoContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder={"BIO"}
+          onChangeText={(value) => setUser({ ...user, bio: value })}
+          placeholderTextColor={"rgba(255,255,255,07)"}
+          underlineColorAndroid={"transparent"}
+        />
+      </View>
+
+      <TouchableOpacity onPress={handlesubmit} style={styles.btnLogin}>
+        <Text style={styles.text}>Sign Up</Text>
+      </TouchableOpacity>
+      {authStore.user ? <Text>helloo</Text> : <Text>byeeeeeeeee</Text>}
+    </ImageBackground>
   );
 };
 
-export default Signup;
+export default observer(Home);
+const styles = StyleSheet.create({
+  backgroundContainer: {
+    width: "100%",
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  logoContainer: {
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  logo: {
+    width: 120,
+    height: 120,
+  },
+  logoText: {
+    color: "white",
+    fontWeight: "500",
+    fontSize: 20,
+    marginBottom: 10,
+    opacity: 0.5,
+  },
+  input: {
+    width: WIDTH - 40,
+    height: 40,
+    borderRadius: 45,
+    borderColor: "grey",
+    borderWidth: 1,
+    fontSize: 16,
+    paddingLeft: 45,
+    backgroundColor: "transparent",
+    color: "rgba(255,255,255,0.7)",
+    marginHorizontal: 25,
+  },
+  inputIcon: {
+    position: "absolute",
+    top: 8,
+    left: 37,
+  },
+  btnEye: {
+    position: "absolute",
+    top: 8,
+    right: 37,
+  },
+  btnLogin: {
+    width: WIDTH - 40,
+    height: 40,
+    borderRadius: 45,
+    fontSize: 16,
+    backgroundColor: "#432577",
+    justifyContent: "center",
+    marginTop: 20,
+  },
+  text: {
+    color: "rgba(255,255,255,0.7)",
+    fontSize: 20,
+    textAlign: "center",
+  },
+});
